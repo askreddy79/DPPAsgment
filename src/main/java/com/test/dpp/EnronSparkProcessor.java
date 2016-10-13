38f6bec5b78842062392cabcca200829f092bbe4
Suneel <askreddy79@gmail.com>
2016-10-13 16:56:01 +0100

    added assembly plugins

--- src/main/java/com/test/dpp/EnronSparkProcessor.java
+++ src/main/java/com/test/dpp/EnronSparkProcessor.java
@@ -2,22 +2,26 @@ package com.test.dpp;
 
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.io.IOException;
+
 /**
  * Created by suneel on 08/10/2016.
  */
 public class EnronSparkProcessor {
-
+    static Logger LOG = Logger.getLogger(EnronSparkProcessor.class);
     public JavaSparkContext sparkContext;
-    public EnronSparkProcessor(String path){
+    public EnronSparkProcessor(String path) throws IOException{
        sparkContext = initializeSpark();
+       Job job = Job.getInstance(sparkContext.hadoopConfiguration());
        JavaPairRDD<Text,BytesWritable> zipFileRDD = sparkContext.newAPIHadoopFile(path,new ZipFileInputFormat().getClass(),Text.class,
-               BytesWritable.class,sparkContext.hadoopConfiguration());
-
+               BytesWritable.class,job.getConfiguration());
 
 
     }
@@ -29,7 +33,12 @@ public class EnronSparkProcessor {
     }
     public static void main(String args[]) {
         String path = args[0];
-       new EnronSparkProcessor(path);
+        try {
+            new EnronSparkProcessor(path);
+        } catch(IOException ex) {
+            LOG.info("Error in main method while processing EnronSpark: " +  ex.getMessage());
+        }
+
     }
 
 
